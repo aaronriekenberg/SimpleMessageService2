a5f76bb412dbcccd4c0b5e151fd528d756ffb7d1
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2015-08-01 10:39:53 -0500

    simplify

--- sms-core/src/main/java/org/aaron/sms/broker/SMSBroker.java
+++ sms-core/src/main/java/org/aaron/sms/broker/SMSBroker.java
@@ -6,6 +6,8 @@ import io.netty.channel.unix.DomainSocketAddress;
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class SMSBroker {
 
@@ -31,43 +33,28 @@ public class SMSBroker {
 
     public static class Builder {
 
-        private final ArrayList<SMSBrokerServer> servers = new ArrayList<>();
-
-        private SMSTopicContainer topicContainer = null;
-
-        public Builder() {
-            reset();
-        }
+        private final ArrayList<Function<SMSTopicContainer, SMSBrokerServer>> createServerFunctions = new ArrayList<>();
 
         public Builder addTCPServer(InetSocketAddress bindAddress) {
-            servers.add(new SMSBrokerTCPServer(getTopicContainer(), bindAddress));
+            createServerFunctions.add((topicContainer) -> new SMSBrokerTCPServer(topicContainer, bindAddress));
             return this;
         }
 
         public Builder addUnixServer(DomainSocketAddress bindAddress) {
-            servers.add(new SMSBrokerUnixServer(getTopicContainer(), bindAddress));
+            createServerFunctions.add((topicContainer) -> new SMSBrokerUnixServer(topicContainer, bindAddress));
             return this;
         }
 
         public SMSBroker build() {
+            final SMSTopicContainer topicContainer = new SMSTopicContainer();
+            final List<SMSBrokerServer> servers = createServerFunctions.stream().map(f ->
+                    f.apply(topicContainer)).collect(Collectors.toList());
+
             final SMSBroker broker = new SMSBroker(servers);
-            reset();
+            createServerFunctions.clear();
             return broker;
         }
 
-        public Builder reset() {
-            servers.clear();
-            topicContainer = null;
-            return this;
-        }
-
-        private SMSTopicContainer getTopicContainer() {
-            if (topicContainer == null) {
-                topicContainer = new SMSTopicContainer();
-            }
-            return topicContainer;
-        }
-        
     }
 
 }
