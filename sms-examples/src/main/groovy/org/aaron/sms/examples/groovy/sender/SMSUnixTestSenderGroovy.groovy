f337d4b1d6faa65c9369018a6882a10d39fdc232
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2016-02-21 10:30:37 -0600

    set thread name

--- sms-examples/src/main/groovy/org/aaron/sms/examples/groovy/sender/SMSUnixTestSenderGroovy.groovy
+++ sms-examples/src/main/groovy/org/aaron/sms/examples/groovy/sender/SMSUnixTestSenderGroovy.groovy
@@ -23,13 +23,14 @@ class SMSUnixTestSenderGroovy {
         log.info "SLEEP_BETWEEN_SENDS_MS = ${SLEEP_BETWEEN_SENDS_MS}"
 
         final List<Thread> threadList = (0..<NUM_SENDERS).collect { i ->
+            String topicName = "test.topic.${i}"
             GroovySenderRunnable runnable =
                     new GroovySenderRunnable(
                             smsConnection: createConnection(),
-                            topicName: "test.topic.${i}",
+                            topicName: topicName,
                             messageSizeBytes: MESSAGE_SIZE_BYTES,
                             sleepBetweenSendsMS: SLEEP_BETWEEN_SENDS_MS)
-            Thread.start runnable.&run
+            Thread.start "sender ${topicName}", runnable.&run
         }
 
         threadList.each { t -> Uninterruptibles.joinUninterruptibly(t) }
